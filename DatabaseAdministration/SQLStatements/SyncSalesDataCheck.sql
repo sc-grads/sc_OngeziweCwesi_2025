d749f971c111be93a45cd6d6c317f0ae6644b69b
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-03-18 15:02:02 +0200

    Verified data consistency between AdventureWorks and AdventureWorks_repl

---
+++ DatabaseAdministration/SQLStatements/SyncSalesDataCheck.sql
@@ -0,0 +1,32 @@
+SELECT TOP (1000) [EmpID]
+        ,[EmpName]
+        ,[SalesNumber]
+        ,[ItemSold]
+    FROM [AdventureWorks2019].[dbo].[Sales]
+
+
+SELECT TOP (1000) [EmpID]
+        ,[EmpName]
+        ,[SalesNumber]
+        ,[ItemSold]
+    FROM [AdventureWorks2019].[dbo].[Sales]
+
+    update Sales set itemsold = 2000 where empid = 3 and salesnumber = 8989
+
+
+
+SELECT TOP (1000) [EmpID]
+        ,[EmpName]
+        ,[SalesNumber]
+        ,[ItemSold]
+    FROM [AdventureWorks2019].[dbo].[Sales]
+
+    select getdate()
+
+SELECT TOP (1000) [EmpID]
+        ,[EmpName]
+        ,[SalesNumber]
+        ,[ItemSold]
+    FROM [AdventureWorks2019].[dbo].[Sales]
+
+    update Sales set itemsold = 3000 where empid = 3 and salesnumber = 8989

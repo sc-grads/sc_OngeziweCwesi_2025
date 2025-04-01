1c0623149dbb500481c5ef3472f98c91b132feb2
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-04-01 12:09:09 +0200

    Update the script

--- Linux/commands.sh
+++ Linux/commands.sh
@@ -4,3 +4,9 @@ echo 'pwd - print working directory'
 echo 'cd - change directory'
 echo 'mkdir - make a new directory'
 echo 'rm - remove files or directories'
+ ls -l Lists directory contents using a long listing format.
+cat Concatenates and displays files.
+clear Clears the screen.
+man command Displays the online manual for command.
+exit Exits the shell or your current session.
+which Locate a command

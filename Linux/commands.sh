3613b359eb45bd4660f289ec9dff4f3e78ebe8bc
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-04-01 15:33:10 +0200

    Update the script

--- Linux/commands.sh
+++ Linux/commands.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 # This script lists basic Linux commands and their descriptions
- ls #list directory contents
- pwd #print working directory
- cd #change directory
- mkdir #make a new directory
- rm #remove files or directories
- ls -l #Lists directory contents using a long listing format.
+
+ls #list directory contents
+pwd #print working directory
+cd #change directory
+mkdir #make a new directory
+rm #remove files or directories
+ls -l #Lists directory contents using a long listing format.
 cat #Concatenates and displays files.
 clear #Clears the screen.
 man #command Displays the online manual for command.
 exit #Exits the shell or your current session.
 which #Locate a command
+q #quit
+space #display the next page
+ls -a #Shows hidden files
+ls -t #List files by time.
+ls -r #Reverse order.
+ls -latr #Long listing including all files reverse sorted by time.
+ls -R #Lists files recursively
+tree -d #List directories only.
+tree -C  #Colorize output.
+ls -d #List directory name, not contents.
+ls --color #Colorize the output.
+rm -r #Remove the directory and its contents recursively.
+rm -f #Force removal and never prompt for confirmation.
+cp source_file destination_file #Copy source_file to destination_file.
+mv #Move or rename files and directories.
+sort file #Sort text in file.
+
+

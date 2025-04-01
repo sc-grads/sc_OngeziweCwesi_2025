b75595a781cd4ad841c567a66bff55a4fe4b6f19
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-04-01 12:15:03 +0200

    Add the new commands

--- Linux/commands.sh
+++ Linux/commands.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 # This script lists basic Linux commands and their descriptions
-echo 'ls - list directory contents'
-echo 'pwd - print working directory'
-echo 'cd - change directory'
-echo 'mkdir - make a new directory'
-echo 'rm - remove files or directories'
- ls -l Lists directory contents using a long listing format.
-cat Concatenates and displays files.
-clear Clears the screen.
-man command Displays the online manual for command.
-exit Exits the shell or your current session.
-which Locate a command
+ ls #list directory contents
+ pwd #print working directory
+ cd #change directory
+ mkdir #make a new directory
+ rm #remove files or directories
+ ls -l #Lists directory contents using a long listing format.
+cat #Concatenates and displays files.
+clear #Clears the screen.
+man #command Displays the online manual for command.
+exit #Exits the shell or your current session.
+which #Locate a command

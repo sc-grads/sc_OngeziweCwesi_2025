30abd157177f16a63a70e43b79a6a62008c4f05b
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-04-01 09:47:08 +0200

    Add Linux folder with bash script containing basic commands

---
+++ Linux/commands.sh
@@ -0,0 +1,6 @@
+# This script lists basic Linux commands and their descriptions
+echo 'ls - list directory contents'
+echo 'pwd - print working directory'
+echo 'cd - change directory'
+echo 'mkdir - make a new directory'
+echo 'rm - remove files or directories'

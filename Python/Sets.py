b8652501886e1eb2d02e88b5d77daaa5aa5ca291
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-05-05 13:06:27 +0200

    Ways to work with Sets

---
+++ Python/Sets.py
@@ -0,0 +1,19 @@
+elements: set = {99, True, 'Bob'}
+
+print(elements)
+
+#Add element
+elements.add('James')
+print(elements)
+
+#Remove element
+elements.remove('James')
+print(elements)
+
+#Pop
+elements.pop()
+print(elements)
+
+#Clear
+elements.clear()
+print(elements)

1be8ea1810c69f17dc8e526d2836f7ee5d4d0000
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-05-06 21:15:25 +0200

    Add the changes about raise

---
+++ Python/Raise.py
@@ -0,0 +1,9 @@
+def check_age(age: int) -> bool:
+    if age < 0:
+        raise ValueError('Not a valid age...')
+    elif age >= 21:
+        print('You are old enough!')
+        return True
+    else:
+        print('You are not old enough...')
+        return False

c6588a14f30df4d3f5a2f310b578cb5e50325fdd
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-05-06 14:29:04 +0200

    Handle Exceptions

--- Python/Exception.py
+++ Python/Exception.py
@@ -1,3 +1,5 @@
+from logging import exception
+import sys
 try:
     result: float = 10/0
     print(result)
@@ -6,3 +8,18 @@ except Exception as e:
 
 
 print('Done')
+
+
+total: float = 0
+while True:
+    user_input: str = input('Enter a number: ')
+
+    if user_input == '0':
+        print(f'Total: {total}')
+        sys.exit()
+
+
+        try:
+            total += float(user_input)
+        except ValueError:
+            print('Please enter a valid number...')

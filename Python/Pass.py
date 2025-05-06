fadc0b4a21ee0faa931abeba0d0832060140ed9a
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-05-06 10:26:51 +0200

    How pass work

---
+++ Python/Pass.py
@@ -0,0 +1,21 @@
+def get_status():
+    pass
+
+
+def connect_to_internet():
+    pass
+
+#------------------------------
+
+number: int = 2
+
+if number > 0:
+    print('More than 0')
+else:
+    pass
+
+
+#-----------------------------
+
+for i in range(3):
+    pass

a92d5d5841c64ec2e9164d12f7403b9061ce20ba
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-05-20 21:19:01 +0200

    MERGE Statement

---
+++ DatabaseAdministration/T-SQL/MergeStatement.sql
@@ -0,0 +1,37 @@
+BEGIN TRAN
+MERGE INTO tblTransaction as T
+USING tblTransactionNew as S
+ON T.EmployeeNumber = S.EmployeeNumber AND T.DateOfTransaction = S.DateOfTransaction
+WHEN MATCHED THEN
+    UPDATE SET Amount = T.Amount + S.Amount
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT ([Amount], [DateOfTransaction], [EmployeeNumber])
+	VALUES (S.Amount, S.DateOfTransaction, S.EmployeeNumber);
+ROLLBACK TRAN
+
+-- tblTransaction (no) - tblTransactionNew (yes)
+-- 1 tblTransaction - 1 tblTransactionNew
+-- 1 tblTransaction - multiple rows TblTransactionNew
+-- Let’s expand our MERGE statement
+SELECT DateOfTransaction, EmployeeNumber, COUNT(*) AS NumberOfRows
+FROM tblTransactionNew 
+GROUP BY DateOfTransaction, EmployeeNumber
+HAVING COUNT(*)>1
+
+BEGIN TRAN
+go
+DISABLE TRIGGER TR_tblTransaction ON dbo.tblTransaction
+GO
+MERGE INTO tblTransaction as T
+USING (SELECT DateOfTransaction, EmployeeNumber, MIN(Amount) as Amount
+      FROM tblTransactionNew
+	  GROUP BY DateOfTransaction, EmployeeNumber) as S
+ON T.EmployeeNumber = S.EmployeeNumber AND
+	T.DateOfTransaction = S.DateOfTransaction
+WHEN MATCHED THEN
+    UPDATE SET Amount = T.Amount + S.Amount
+WHEN NOT MATCHED THEN
+	INSERT (Amount, DateOfTransaction, EmployeeNumber)
+	VALUES (S.Amount, S.DateOfTransaction, S.EmployeeNumber)
+	OUTPUT deleted.*, inserted.*;
+ROLLBACK TRAN

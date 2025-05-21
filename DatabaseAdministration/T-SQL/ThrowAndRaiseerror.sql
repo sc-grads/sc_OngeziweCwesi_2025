991fdf9231b984e41198f8de467f6f1c7aa90138
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-05-21 08:50:19 +0200

    THROW and RAISEERROR

--- DatabaseAdministration/T-SQL/ThrowAndRaiseerror.sql
+++ DatabaseAdministration/T-SQL/ThrowAndRaiseerror.sql
@@ -5,9 +5,11 @@ go
 create proc AverageBalance(@EmployeeNumberFrom int, @EmployeeNumberTo int, @AverageBalance int OUTPUT) as
 begin
 	SET NOCOUNT ON
-	declare @TotalAmount money
+	declare @TotalAmount decimal(5,2)
 	declare @NumOfEmployee int
 	begin try
+		print 'The employee numbers are from ' + convert(varchar(10),@EmployeeNumberFrom) 
+		      + ' to ' + convert(varchar(10),@EmployeeNumberTo)
 		select @TotalAmount = sum(Amount) from tblTransaction
 		where EmployeeNumber between @EmployeeNumberFrom and @EmployeeNumberTo
 		select @NumOfEmployee = count(distinct EmployeeNumber) from tblEmployee
@@ -17,12 +19,17 @@ begin
 	end try
 	begin catch
 		set @AverageBalance = 0
-		SELECT ERROR_MESSAGE() AS ErrorMessage, ERROR_LINE() as ErrorLine,
-			   ERROR_NUMBER() as ErrorNumber, ERROR_PROCEDURE() as ErrorProcedure,
-			   ERROR_SEVERITY() as ErrorSeverity,  
-			    
-			   ERROR_STATE() as ErrorState
-		RETURN 1
+		if ERROR_NUMBER() = 8134 -- @@ERROR
+		begin
+			set @AverageBalance = 0
+			print 'There are no valid employees in this range.'
+			Return 8134
+		end
+		else
+		    declare @ErrorMessage as varchar(255)
+			select @ErrorMessage = error_Message()
+			raiserror (@ErrorMessage, 16, 1)
+		select 'Hi There'
 	end catch
 end
 go
@@ -32,10 +39,8 @@ select @AvgBalance as Average_Balance, @ReturnStatus as Return_Status
 GO
 DECLARE @AvgBalance int, @ReturnStatus int
 execute @ReturnStatus = AverageBalance 223, 227, @AvgBalance OUTPUT
-select @AvgBalance as Average_Balance, @ReturnStatus as Return_Status
+select @AvgBalance as Average_Balance, @ReturnStatus as Return_Status, 'Error did not stop us' as myMessage
 GO
 DECLARE @AvgBalance int, @ReturnStatus int
 exec @ReturnStatus = AverageBalance @EmployeeNumberFrom = 323, @EmployeeNumberTo = 327, @AverageBalance = @AvgBalance OUTPUT
 select @AvgBalance as Average_Balance, @ReturnStatus as Return_Status
-
-SELECT TRY_CONVERT(int, 'two')

2b64bdf88c5f14550d1d53edbc4f2d4c3b753a76
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-08-11 13:53:22 +0200

    Addition Calculator

--- C#/MyAdditionCalculator/Program.cs
+++ C#/MyAdditionCalculator/Program.cs
@@ -1,12 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Prints out whatever is inside of ()
-Console.WriteLine("Enter something");
 
+
+//datatype variableName = initial value;
+int myNumber = 0;
+int myNumber2 = 0;
+
+// Prints out whatever is inside of ()
+Console.WriteLine("Enter a whole number");
 
 //takes the user input and stores it
 //Variable with the name "userInput" and the data type "string"
 //strings are used for text
 string userInput = Console.ReadLine();
-Console.WriteLine("You entered " + userInput);
+//myNumber = int.Parse(Console.ReadLine());
+myNumber = int.Parse(userInput);
+
+Console.WriteLine("Enter a whole number");
+userInput = Console.ReadLine();
+myNumber2 = int.Parse(userInput);
+
+int sum = myNumber + myNumber2;
+
+Console.WriteLine("The result of " + myNumber + " and "+ myNumber2 + " is " + sum);
 Console.ReadKey();

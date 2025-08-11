ff8e9060e8edd1a6e595e0e5a3789f51d0dbd913
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-08-11 15:35:10 +0200

    Implicit and Explicit conversion

---
+++ C#/implicitConversion/Program.cs
@@ -0,0 +1,37 @@
+﻿//implicit conversion
+
+using System.Runtime.CompilerServices;
+
+int myInt = 13;
+double myDouble = myInt;
+
+long myLong = myInt;
+
+myLong = 132345098312389013;
+
+float myFloat = 123.123f;
+
+myDouble = myFloat;
+
+//Explicit conversion (casting)
+
+myLong = 1323450;
+
+int myInt2 = (int)myLong;
+myInt2 =(int)myDouble;
+
+myFloat =(float)myDouble;
+
+
+
+//Conversion Helpers Parse and Convert
+
+string numberString = "123";
+int result = int.Parse(numberString);
+
+string myBoolString = "true";
+bool myBool = Convert.ToBoolean(myBoolString);
+Console.WriteLine("myBool is "+ myBool);
+
+
+Console.ReadKey();

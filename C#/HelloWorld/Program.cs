6380527cdaf58ad0e644bc5b2d0930e94c2809ef
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-08-11 13:06:43 +0200

    C# BASICS'

---
+++ C#/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// declare a variable
+string myFriendsName;
+// assign a value to the myFriendsName variable
+myFriendsName = "Qhama";
+
+// use/access the variable
+Console.WriteLine(myFriendsName);
+
+Console.ReadKey();
---
+++ C#/MyAdditionCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Prints out whatever is inside of ()
+Console.WriteLine("Enter something");
+
+
+//takes the user input and stores it
+//Variable with the name "userInput" and the data type "string"
+//strings are used for text
+string userInput = Console.ReadLine();
+Console.WriteLine("You entered " + userInput);
+Console.ReadKey();

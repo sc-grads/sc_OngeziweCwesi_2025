4a33ed6d7e768c8e1449e696cdb84d306912a397
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-08-11 13:21:41 +0200

    Data Types

---
+++ C#/DataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Reference Type
+string myName = "Ongeziwe";
+
+//Value Type
+int myAge = 35;
+//double pi = -3.14;
+
+byte age = 255;
+
+short linkedinConnections = 32550;
+
+long phoneNumber = 0721707197;
+
+float pi = 3.14f;
+
+Console.ReadKey();

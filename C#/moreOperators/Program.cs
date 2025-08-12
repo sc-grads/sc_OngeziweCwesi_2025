832f770a7acbbad46dedc9100d441e2d7f071b1c
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-08-12 10:26:28 +0200

    More Operators

---
+++ C#/moreOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 10;
+double price = 19.95;
+string name = "Ongeziwe";
+
+//Interpolation
+Console.WriteLine($"The number is {num}");
+
+
+//String concatination
+Console.WriteLine("The number is "+ num);
+
+//String formatting
+Console.WriteLine("The number is {0}, and the price is {1}, and the name is {2}", num, price, name);
+
+Console.ReadKey();

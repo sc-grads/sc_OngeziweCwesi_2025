704a913f69fdd3e7d9682df58aedd809adcd8584
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-08-13 08:32:36 +0200

    Loop Basics

---
+++ C#/loopBasics/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//in strings \ is an "Escape Character"
+// \n for "new line"
+// \r - carriage return
+string myString = "Hi\nHi";
+
+for (int counter =10; counter >= 0; counter--)
+{
+    Console.WriteLine("Counter is "+ counter);
+    //Console.WriteLine(myString);
+    Thread.Sleep(1000);
+}
+
+Console.ReadKey();

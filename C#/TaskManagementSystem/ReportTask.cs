42a7f8b9b486ef916bfe0e88b36d42eede93ff09
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-09-04 10:57:32 +0200

    Task Management System course

---
+++ C#/TaskManagementSystem/ReportTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem
+{
+    internal class ReportTask : ITask<string>
+    {
+        public string ReportName {get;set;}
+
+        public string Perform()
+        {
+            return $"Report {ReportName} generated successfully.";
+        }
+
+    }
+}
---
+++ C#/TaskManagementSystem/TaskProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem
+{
+    internal class TaskProcessor<TTask, TResult> where TTask : ITask<TResult>
+    {
+        private TTask task;
+
+        public TaskProcessor(TTask task) 
+        { 
+            this.task = task;
+        }
+
+        public TResult Execute()
+        {
+            return task.Perform();
+        }
+    }
+}

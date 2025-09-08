14c2fa10a377a4e7b37481656952897f92800792
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-09-08 08:45:23 +0200

    Domain Test class

---
+++ C#/Domain.Test/Flight.cs
@@ -0,0 +1,30 @@
+﻿using Domains;
+
+namespace Domain
+{
+    
+        public class Flight
+        {
+
+        List<Booking> bookingList = new();
+        public IEnumerable<Booking> BookingList => bookingList;
+        public int RemainingNumberOfSeats { get; set; }
+            public Flight(int seatCapacity)
+            {
+            RemainingNumberOfSeats = seatCapacity;
+            }
+
+            public object? Book(string passengerEmail, int numberOfSeats)
+            {
+            if (numberOfSeats > this.RemainingNumberOfSeats)
+                return new OverbookingError();
+
+            RemainingNumberOfSeats -= numberOfSeats;
+
+            bookingList.Add(new Booking(passengerEmail, numberOfSeats));
+
+            return null;
+            }
+        }
+    
+}

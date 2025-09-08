33eeb595a8323031d4d0ecce6113174d82c87e96
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-09-08 08:45:45 +0200

    Domain Tests class

---
+++ C#/Domain.Tests/Flight.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Tests
+{
+    public class Flight
+    {
+        public int RemainingNumberOfSeats {  get; set; }
+        public Flight(int seatCapacity) 
+        {
+
+        }
+
+        public void Book(string v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

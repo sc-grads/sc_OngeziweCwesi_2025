91d9e46ade14c4e6f79d955e22ed84717eae0aef
ongeziwecwesi2003 <ongeziwe.cwesi@sambeconsulting.com>
2025-09-08 08:46:38 +0200

    Flight class

---
+++ C#/Flight/FlightSpecifications.cs
@@ -0,0 +1,50 @@
+﻿using Domain;
+using Domains;
+using FluentAssertions;
+
+namespace FlightTest
+{
+    public class FlightSpecifications
+    {
+        [Theory]
+        [InlineData(3,1,2)]
+        [InlineData(6, 3, 3)]
+        [InlineData(10, 6, 4)]
+        public void Booking_reduces_the_number_of_seats(int seatCapacity, int numberOfSeats, int remainingNumberOfSeats)
+        {
+            var flight = new Flight(seatCapacity: seatCapacity);
+            flight.Book("janick@tutorial.com", numberOfSeats);
+
+            flight.RemainingNumberOfSeats.Should().Be(remainingNumberOfSeats);
+        }
+
+        [Fact]
+        public void Avoids_overbooking()
+        {
+            //Given
+            var flight = new Flight(seatCapacity: 3);
+
+            //When
+            var error = flight.Book("janick@tutorial.com", 4);
+
+            //Then
+            error.Should().BeOfType<OverbookingError>();
+        }
+
+        [Fact]
+        public void Books_flights_successfully()
+        {
+            var flight = new Flight(seatCapacity: 3);
+            var error = flight.Book("janick@tutorial.com", 1);
+            error.Should().BeNull();
+        }
+
+        [Fact]
+        public void Remembers_bookings()
+        {
+            var flight = new Flight(seatCapacity: 150);
+            flight.Book(passengerEmail: "a@b.com", numberOfSeats: 4);
+            flight.BookingList.Should().ContainEquivalentOf(new Booking("a@b.com",4));
+        }
+    }
+}
